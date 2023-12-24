f1437469af0c5eef61a04e3fd36f80369b632f36
jbernouli <jahiz.ahmed@hotmail.com>
2023-12-24 18:18:03 -0500

    All tf lite for micro files for environment setup generic cortex-m

---
+++ examples/micro_speech/models/audio_preprocessor_int8_model_data.h
@@ -0,0 +1,4 @@
+#include <cstdint>
+
+extern const unsigned int g_audio_preprocessor_int8_model_data_size;
+extern const unsigned char g_audio_preprocessor_int8_model_data[];
---
+++ examples/micro_speech/testdata/noise_1000ms_audio_data.h
@@ -0,0 +1,4 @@
+#include <cstdint>
+
+extern const unsigned int g_noise_1000ms_audio_data_size;
+extern const int16_t g_noise_1000ms_audio_data[];
